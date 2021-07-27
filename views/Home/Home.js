cd24e285438ab7d2ff15440b53f6642664f4d3fa
Michael Curran <mcurran1722@gmail.com>
2021-07-26 23:10:13 -0600

    adding signed out view

--- views/Home/Home.js
+++ views/Home/Home.js
@@ -1,30 +1,33 @@
-import Head from "next/head";
-import Link from "next/link";
-import styles from "./Home.module.scss";
-import { SignedIn, SignedOut } from "@clerk/clerk-react";
-import Practice from "../Practice/Practice";
-import { useUser } from "@clerk/clerk-react";
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from './Home.module.scss';
+import { SignedIn, SignedOut } from '@clerk/clerk-react';
+import Practice from '../Practice/Practice';
+import { useUser } from '@clerk/clerk-react';
 
 
-const SignedInPractice = ({...props}) => {
-  const user = useUser();
-  return  <Practice user={user}/> ;
+const SignedInPractice = ({ ...props }) => {
+    const user = useUser();
+    return <Practice user={user}/>;
 };
   
-  const Home = ({...props}) => (
+const Home = ({ ...props }) => (
     <div className={styles.container}>
-      <Head>
-        <title>Type Warrior</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        ></meta>
-      </Head>
-      <SignedIn>
-        <SignedInPractice {...props}/>
-      </SignedIn>
+        <Head>
+            <title>Type Warrior</title>
+            <link rel="icon" href="/favicon.ico" />
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1.0"
+            ></meta>
+        </Head>
+        <SignedIn>
+            <SignedInPractice {...props}/>
+        </SignedIn>
+        <SignedOut>
+            <Practice />
+        </SignedOut>
     </div>
-  );
+);
   
-  export default Home;
+export default Home;
