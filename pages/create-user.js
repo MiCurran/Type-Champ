a8b6c75582dc46d439e5103e55b6027df287bcae
Michael Curran <mcurran1722@gmail.com>
2021-07-29 23:35:23 -0600

    add user object to signup callback page

--- pages/create-user.js
+++ pages/create-user.js
@@ -1,5 +1,12 @@
+import { SignedIn } from '@clerk/clerk-react';
 import CreateUser from '../views/CreateUser/CreateUser';
+import { useUser } from '@clerk/clerk-react';
+import { WithUser } from '@clerk/clerk-react';
+const CreateUserPage = (props) => {
+    return (
+        <SignedIn><CreateUser /></SignedIn>
+    );
+};
 
-const CreateUserPage = (props) => (<CreateUser {...props} />);
 
 export default CreateUserPage;
--- views/CreateUser/CreateUser.js
+++ views/CreateUser/CreateUser.js
@@ -1,19 +1,30 @@
 import Head from 'next/head';
-  
-const CreateUser = ({ ...props }) => (
-    <div className="container">
-        <Head>
-            <title>Type Warrior</title>
-            <link rel="icon" href="/favicon.ico" />
-            <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1.0"
-            ></meta>
-        </Head>
-        <div>
-            create user page
+import { useUser } from '@clerk/clerk-react';
+import { Button, Heading, Input } from '@chakra-ui/react';
+import { useState } from 'react';
+
+const CreateUser = ({ ...props }) => {
+    const user = useUser();
+    const [displayName, setDisplayName] = useState('');
+    return (
+        <div className="container">
+            <Head>
+                <title>Type Warrior</title>
+                <link rel="icon" href="/favicon.ico" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                ></meta>
+            </Head>
+            <div>
+                <Heading as="h1">Create User</Heading>
+                <Heading as="h3">Choose a display name</Heading>
+                <Input onBlur={(e) => setDisplayName(e.target.value)}></Input>
+                {/* <Button onClick={() => console.log(user.id)}>log</Button> */}
+                <Button onClick={() => console.log(`creating user ${displayName} for id ${user.id}` )}>Create!</Button>
+            </div>
         </div>
-    </div>
-);
+    );
+};
   
 export default CreateUser;
